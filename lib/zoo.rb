5838d236405e472c1a7224d2e4a469d6a90be5a2
VictorVitkov <victor.vitkov@gmail.com>
2019-06-06 11:30:53 -0400

    Initialize Zoo Class

--- lib/zoo.rb
+++ lib/zoo.rb
@@ -1,3 +1,18 @@
 class Zoo
+  attr_reader :cages, :employees
 
+  def initialize(name, season_opening_date, season_closing_date)
+    @name = name
+    @season_opening_date = season_opening_date
+    @season_closing_date = season_closing_date
+    @cages = []
+    @employees = []
+    10.times do
+      @cages << Cage.new
+    end
+  end
+
+  def add_employee(worker)
+    @employees << worker
+  end
 end
