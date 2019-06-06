34917328a3e4356bcf855834db20642980e842d1
VictorVitkov <victor.vitkov@gmail.com>
2019-06-06 10:55:08 -0400

    Initialize Cage Class

--- lib/cage.rb
+++ lib/cage.rb
@@ -1,3 +1,16 @@
 class Cage
+  attr_accessor :animal
 
+  def initialize
+    @animal = nil
+  end
+
+  def empty?
+    @animal.nil?
+    # if @animal == nil
+    #   true
+    # else
+    #   false
+    # end
+  end
 end
